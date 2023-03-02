94603e0f5665b33c4af4b6d0c7b0ca4dd164aab4
William Lardner <william.lardner0109@gmail.com>
2023-03-02 16:51:16 +0000

    Add version 1.2 files

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// Do not mess with this file unless you know what you're doing :P
+
+$(document).on('click', 'a[href^="#"]', function (e) {
+	e.preventDefault();
+
+	$('html, body').animate({
+		scrollTop: $($.attr(this, 'href')).offset().top
+	}, 900);
+});
+
+// This is for the click to copy
+let t;
+$(document).ready(()=>{
+	t = $(".ip").html();
+})
+$(document).on("click",".ip",()=>{
+	let copy = document.createElement("textarea");
+	copy.style.position = "absolute";
+	copy.style.left = "-99999px";
+	copy.style.top = "0";
+	copy.setAttribute("id", "ta");
+	document.body.appendChild(copy);
+	copy.textContent = t;
+	copy.select();
+	document.execCommand("copy");
+	$(".ip").html("<span class='extrapad'>IP copied!</span>");
+	setTimeout(function(){
+		$(".ip").html(t);
+		var copy = document.getElementById("ta");
+		copy.parentNode.removeChild(copy);
+	},1000);
+});
+
+// This is to fetch the player count
+$(document).ready(()=>{
+  const ip = $(".sip").attr("data-ip");
+  const port = $(".sip").attr("data-port");
+
+  $.get(`https://mcapi.us/server/status?ip=${ip}&port=${port}`, (result)=>{
+    if (result.online) {
+      $(".sip").html(result.players.now);
+    } else {
+      $(".playercount").html("Server isn't online!");
+    }
+  });
+
+  setInterval(()=>{
+    $.get(`https://mcapi.us/server/status?ip=${ip}&port=${port}`, (result)=>{
+      if (result.online) {
+        $(".sip").html(result.players.now);
+      } else {
+        $(".playercount").html("Server isn't online!");
+      }
+    });
+  }, 500);
+});
